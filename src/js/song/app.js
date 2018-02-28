dde52aa057930ed4e534542cf55c0ef0379b45fc
萧泳森 <603540681@qq.com>
2018-02-28 13:30:56 +0800

    进入播放页面，自动播放音乐

--- src/js/song/app.js
+++ src/js/song/app.js
@@ -1,26 +1,36 @@
 {
   let view = {
-    el: '',
+    el: '#page',
     init(){
       this.$el = $(this.el)
-    }
+    },
+    render(data){
+      console.log(data)
+      let {song} = data
+      this.$el.find('.background').css(`background-image`,`url(${song.cover})`)
+      this.$el.find('.cover').attr('src',song.cover)
+      this.$el.find('audio').attr('src',song.url)
+    },
   }
 
   let model = {
     data:{
-      id: '',
-      name: '',
-      singer: '',
-      url: ''
+      song:{
+        id: '',
+        name: '',
+        singer: '',
+        url: '',
+        cover: '',
+      },
+      status: 'playing'
     },
     setSongId(id){
-      this.data.id = id
+      this.data.song.id = id
     },
     getSong(id){
       var query = new AV.Query('Song');
-      query.get(id).then((song)=>{
-        Object.assign(this.data,song.attributes)
-        console.log(this.data)
+      return query.get(id).then((song)=>{
+        return Object.assign(this.data.song, song.attributes)
       }, function (error) {
         console.log(error)
       })
@@ -33,7 +43,10 @@
       this.view.init()
       this.model = model
       this.getSongID()
-      this.model.getSong(this.model.data.id)
+      this.model.getSong(this.model.data.song.id).then((song)=>{
+        this.view.render(this.model.data)
+      })
+      this.bindEvents()
     },
     getSongID(){
       let search = window.location.search
@@ -56,6 +69,21 @@
       this.model.setSongId(id)
       return id
     },
+    bindEvents(){
+      this.view.$el.on('click','main',(e)=>{
+        if(this.model.data.status === 'playing'){
+          this.model.data.status = 'paused'
+          this.view.$el.find('.icon').show()
+          this.view.$el.find('.cover').addClass('paused')
+          myAudio.pause()
+        }else if(this.model.data.status === 'paused'){
+          this.model.data.status = 'playing'
+          this.view.$el.find('.icon').hide()
+          this.view.$el.find('.cover').removeClass('paused')
+          myAudio.play()
+        }
+      })
+    }
   }
 
   controller.init(view, model)
